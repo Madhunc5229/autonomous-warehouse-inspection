804aef9365cc0482993c82f5504c75ad970633e1
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-12-14 12:57:40 -0500

    T16 updated botInspector hpp file

--- include/botInspector.h
+++ include/botInspector.h
@@ -1,40 +1,49 @@
-/**
- * @file botInspector.h
- * @author Madhu Narra Chittibabu (mnarrach@umd.edu)
- * @brief This file contains all methods related to the turtle bot
- * @version 0.1
- * @date 2022-12-02
- * 
- * @copyright Copyright (c) 2022
- * 
- */
-#ifndef INCLUDE_BOTINSPECTOR_H_
-#define INCLUDE_BOTINSPECTOR_H_
+#pragma once
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <chrono>
 
-#include <rclcpp/rclcpp.hpp>
 
-/**
- * @brief This class helps in interfacing with the turtlebot
- * 
- */
-class BotInspector {
+using std::placeholders::_1;
+using PUB = rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr;
+using TIMER = rclcpp::TimerBase::SharedPtr;
+using ODOMETRY = nav_msgs::msg::Odometry;
+using namespace std::chrono_literals;
+using std::chrono::duration;
+using BOT_POSE = geometry_msgs::msg::PoseStamped;
+
+class BotInspector : public rclcpp::Node {
+
     public:
-        /**
-         * @brief Construct a new Bot Inspector object
-         * 
-         */
         BotInspector();
+
+
+        void inspection();
+
+        void moveToLocation();
+
+        void resumeInspection();
+
+
     private:
-        rclcpp::Publisher<geometry_msgs::msgs::Twist>::SharedPtr publisher_;
-        std::array<double> aruco_locations;
-        gemetry_msgs::Twist msg;
+        geometry_msgs::msg::Pose current_position;
+        geometry_msgs::msg::Pose next_position;
+        ODOMETRY::SharedPtr odom_msg_;
+        PUB pose_publisher_;
+        TIMER timer_;
+        bool pose_flag = false;
 
-        /**
-         * @brief this is a callback function
-         * 
-         */
-        void inspectorCallBack();
+        void inspectionCallback(const ODOMETRY::SharedPtr odom_msg_i);
+        void resumeInspectionCallback(const ODOMETRY::SharedPtr odom_msg_r);
 
-}
+        float goal_x = -2.5;
+        float goal_y = -3.5;
 
-#endif // INCLUDE_BOTINSPECTOR_H_
+
+
+};
