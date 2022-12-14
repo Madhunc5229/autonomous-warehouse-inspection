1f8ec32b4bf875da85fcf9bd4ee23cb252c4d110
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-12-14 16:08:26 -0500

    T17 Beacon header file updates with subscriber

--- include/beacon.h
+++ include/beacon.h
@@ -1,44 +1,32 @@
-/**
- * @file beacon.h
- * @author Madhu Narra Chittibabu (mnarrach@umd.edu)
- * @brief This file contains all methods related to the aruco markers
- * @version 0.1
- * @date 2022-12-02
- * 
- * @copyright Copyright (c) 2022
- * 
- */
-#ifndef INCLUDE_BEACON_H_
-#define INCLUDE_BEACON_H_
+#pragma once
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <chrono>
 
-#include <rclcpp/rclcpp.hpp>
 
-/**
- * @brief This class helps in interfacing with the aruco markers
- * 
- */
-class Beacon {
+using std::placeholders::_1;
+using PUB = rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr;
+using TIMER = rclcpp::TimerBase::SharedPtr;
+using ODOMETRY = nav_msgs::msg::Odometry;
+using namespace std::chrono_literals;
+using std::chrono::duration;
+using BOT_POSE = geometry_msgs::msg::PoseStamped;
+
+using ARUCO_TYPE = geometry_msgs::msg::Pose;
+
+
+
+class Beacon : public rclcpp::Node{
     public:
-        /**
-         * @brief Construct a new Beacon object
-         * 
-         */
         Beacon();
-    private:
-        /**
-         * @brief This method is used to broadcast marker position
-         * 
-         */
-        beaconBroadcaster();
-        /**
-         * @brief This method is used to get transform of marker w.r.t world
-         * 
-         */
-        beaconTransform();
-        
-        std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-        geometry_msgs::msg::TransformedStamped t;
-        tf2::Quaternion q_rpy;
-}
 
-#endif // INCLUDE_BOTINSPECTOR_H_
+    private:
+        void arucoCallback(const ARUCO_TYPE::SharedPtr aruco_msg);
+        
+
+};
