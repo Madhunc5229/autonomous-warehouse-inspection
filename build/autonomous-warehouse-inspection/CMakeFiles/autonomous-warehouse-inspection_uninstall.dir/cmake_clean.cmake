60ff6cedcc2a04e88ffe358e50ba956a74fea94b
Madhunc5229 <madhunc117@gmail.com>
2022-12-15 12:41:48 -0500

    deleted log files

--- build/autonomous-warehouse-inspection/CMakeFiles/autonomous-warehouse-inspection_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/autonomous-warehouse-inspection_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/autonomous-warehouse-inspection_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/autonomous-warehouse-inspection/colcon_command_prefix_test.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/mnc/ros2_ws/src/autonomous-warehouse-inspection/install/autonomous-warehouse-inspection/share/autonomous-warehouse-inspection/package.sh"
