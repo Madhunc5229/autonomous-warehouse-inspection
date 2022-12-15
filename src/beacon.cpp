9d97b1ca0e56690b87ea1f2fac38559bfce7f574
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-12-14 23:24:31 -0500

    T21 cpplint results

--- src/beacon.cpp
+++ src/beacon.cpp
@@ -1,16 +1,29 @@
+/**
+ * @file beacon.cpp
+ * @author Madhu Narra Chittibabu (mnarrach@umd.edu)
+ * @author Koundinya Vinnakota (vkd@umd.edu)
+ * @author Sharmitha Ganeshan (mnarrach@umd.edu)
+ * @brief This file contains subscriber to Aruco marker.
+ * @version 0.1
+ * @date 2022-12-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include "../include/beacon.h"
 
-Beacon::Beacon() : Node("beacon"){
-   auto beacon_ = this->create_subscription<ARUCO_TYPE>("aruco_markers",10, std::bind(&Beacon::arucoCallback, this , _1));
+Beacon::Beacon() : Node("beacon") {
+  auto beacon_ = this->create_subscription<ARUCO_TYPE>(
+      "aruco_markers", 10, std::bind(&Beacon::arucoCallback, this, _1));
 }
 
-void Beacon::arucoCallback(const ARUCO_TYPE::SharedPtr aruco_msg){
-    RCLCPP_INFO_STREAM(this->get_logger(), aruco_msg);
-
+void Beacon::arucoCallback(const ARUCO_TYPE::SharedPtr aruco_msg) {
+  RCLCPP_INFO_STREAM(this->get_logger(), aruco_msg);
 }
 
-int main(){
-    rclcpp::spin(std::make_shared<Beacon>());
+int main() {
+  rclcpp::spin(std::make_shared<Beacon>());
 
-    rclcpp::shutdown();
+  rclcpp::shutdown();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
-#include "../include/botInspector.h"
+/**
+ * @file main.cpp
+ * @author Madhu Narra Chittibabu (mnarrach@umd.edu)
+ * @author Koundinya Vinnakota (vkd@umd.edu)
+ * @author Sharmitha Ganeshan (mnarrach@umd.edu)
+ * @brief Main file to execute the bot's movements.
+ * @version 0.1
+ * @date 2022-12-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include "../include/beacon.h"
+#include "../include/botInspector.h"
 
-int main(int argc, char * argv[]){
-    rclcpp::init(argc,argv);
-    BotInspector inspector;
-    // Beacon beacon;
+int main(int argc, char* argv[]) {
+  rclcpp::init(argc, argv);
+  BotInspector inspector;
+  // Beacon beacon;
 
-    inspector.moveToLocation();
-    // std::cout<<"dne"<<"/n";
-    inspector.rotateInspector();
-    inspector.resumeInspection();
+  inspector.moveToLocation();
+  // std::cout<<"dne"<<"/n";
+  inspector.rotateInspector();
+  inspector.resumeInspection();
 
-    rclcpp::shutdown();
-    
+  rclcpp::shutdown();
 }
-
-
-
-
-
-
