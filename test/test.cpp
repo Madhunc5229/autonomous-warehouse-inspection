92a90970291d58342625539ade683b1f028b7565
Madhunc5229 <madhunc117@gmail.com>
2022-12-14 23:59:27 -0500

    T22 ADDED TEST CASE

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,38 @@
 #include <gtest/gtest.h>
+#include <stdlib.h>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include <chrono>
+#include <cstdlib>
+#include <rclcpp/rclcpp.hpp>
+
+
+class TaskPlanningFixture : public testing::Test {
+ public:
+  TaskPlanningFixture() : node_(std::make_shared<rclcpp::Node>("basic_test")) {
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH CONSTRUCTOR!!");
+  }
+
+  void SetUp() override {
+    // Setup things that should occur before every test instance should go here
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH SETUP!!");
+  }
+
+  void TearDown() override { std::cout << "DONE WITH TEARDOWN" << std::endl; }
+
+ protected:
+  rclcpp::Node::SharedPtr node_;
+};
+
+
+TEST(TaskPlanningFixture, goalPoseCheck) {
+  
+}
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  int result = RUN_ALL_TESTS();
+  rclcpp::shutdown();
+  std::cout << "DONE SHUTTING DOWN ROS" << std::endl;
+  return result;
 }
