0d9620a3da1c4002dae3344e645e9b950566a899
Madhunc5229 <madhunc117@gmail.com>
2022-12-15 20:35:07 -0500

    T17 added main fiels

---
+++ src/launchInspector.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * @author Madhu Narra Chittibabu (mnarrach@umd.edu)
+ * @author Koundinya Vinnakota (vkd@umd.edu)
+ * @author Sharmitha Ganeshan (mnarrach@umd.edu)
+ * @brief Main file to execute the bot's movements.
+ * @version 0.1
+ * @date 2022-12-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "../include/botInspector.hpp"
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  BotInspector inspector;
+  inspector.setGoal(3.0, 3.0);
+  inspector.moveToLocation();
+  inspector.rotateInspector();
+  inspector.setGoal(3.0,-3.0);
+  inspector.moveToLocation();
+  inspector.rotateInspector();
+  inspector.setGoal(-3.0,3.0);
+  inspector.moveToLocation();
+  inspector.rotateInspector();
+  
+  inspector.resumeInspection();
+
+  rclcpp::shutdown();
+}
